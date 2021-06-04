76e4b9697d24f732bf4315470cfc6aacbdf46f34
Abiola Adeyemi <ifeobayemi@gmail.com>
2021-06-04 11:10:59 +0100

    Add profile name

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+document.getElementById("profile").addEventListener('click', function() {
+    const userName = prompt("Welcome, what's your name?")
+    this.textContent = userName
+})
+
+// document.getElementById("calcBill").addEventListener('click', function() {
+
+// })
+
+// 1. Days spent on admission in the hospital 
+// 2. Medication charges 
+// 3. Surgical Charges 
+// 4. Diagnostic Lab fees 
+// 5. How many meals were bought from the hospital? 
